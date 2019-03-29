02b3bcb7d35ab0866011913e5e7fc35a1d40b3c6
Liu Zhang <liuzhang@99xs.com>
2019-03-29 18:06:25 +0800

    栈

---
+++ stack/C/stack.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+// InitStack(*s)
+// DestroyStack(*s)
+// ClearStack(*s)
+// StackEmpty(*s)
+// GetTop(*s, *e)
+// Push(*s, e)
+// Pop(*s, *e)
+// StackLength(S)
+
+
+struct node {
+	 int data;
+	 struct node *next;
+};
+
+int main()
+{
+	printf("ddddddd");
+	return 0;
+}
